95f2b377c349599bd3300c926aa910737d975bdd
cedric-watkins <ced.wat1@gmail.com>
2024-08-05 22:24:58 -0600

    created a simple heads or tails program using the random module

---
+++ heads_or_tails.py
@@ -0,0 +1,10 @@
+import random
+
+# Generate a random integer between 1 and 2
+random_number = random.randint(1, 2)
+
+# Check the random number and print the result
+if random_number == 1:
+    print("Heads")
+else:
+    print("Tails")
